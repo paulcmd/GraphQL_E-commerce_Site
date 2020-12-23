b4952e0529bf535ecfef68df734172b4cc39e38f
paulcmd <choxmail.pm@gmail.com>
2020-12-22 23:55:07 -0700

    installed material-ui

---
+++ src/components/layout/header/HeaderStyles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    btnSignup:{
+        color: "white"
+    },
+    btnLogin:{
+        color: "white"
+    }
+  }));
+

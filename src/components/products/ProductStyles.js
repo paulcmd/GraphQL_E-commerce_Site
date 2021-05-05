a631149517ed1dbfdaffab5f96a95b5abe971ea4
paulcmd <choxmail.pm@gmail.com>
2021-05-05 03:03:34 -0700

    added stock images

--- src/components/products/ProductStyles.js
+++ src/components/products/ProductStyles.js
@@ -2,12 +2,16 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles(() => ({
     root: {
-        maxWidth: 280
+        maxWidth: 280,
+        flexGrow: 1
     },
     media: {
         height: 230
     },
     gridBox: {
+        margin: "auto"
+    },
+    gridCard: {
         margin: "auto"
     }
 }))
--- src/components/products/dummyData.jsx
+++ src/components/products/dummyData.jsx
@@ -4,32 +4,32 @@ import phone from './phone-image.jpg'
 export const products = [
     {
         id: '1',
-        name: 'phone1',
+        name: 'samsung',
         price: 30000,
         image: 'https://www.socialnetlink.org/wp-content/uploads/2020/10/samsung-galaxy-s20fe-test-live-02.jpg'
     },
     {
         id: '2',
-        name: 'phone2',
+        name: 'LG',
         price: 30000,
-        image: 'https://cdn.pocket-lint.com/r/s/1200x/assets/images/153997-phones-review-hands-on-samsung-galaxy-s20-fe-image1-n1j5wpamd4.JPG'
+        image: 'https://www.cnet.com/a/img/sBg0wK1pbcUbu6qo8E4btsBNo7s=/980x0/2020/07/12/cca8f9ad-28dd-4714-b0e8-8175dbec5161/lg-velvet-3950.jpg'
     },
     {
         id: '3',
-        name: 'phone3',
+        name: 'iPhone',
         price: 30000,
-        image: {phone}
+        image: 'https://media.wired.com/photos/5b22c5c4b878a15e9ce80d92/master/w_2560%2Cc_limit/iphonex-TA.jpg'
     },
     {
         id: '4',
-        name: 'phone4',
+        name: 'Huawei',
         price: 30000,
-        image: {phone}
+        image: 'https://consumer.huawei.com/content/dam/huawei-cbg-site/en/mkt/pdp/phones/p30/P30_share-img.jpg'
     },
     {
         id: '5',
-        name: 'phone5',
+        name: 'Google Pixel',
         price: 30000,
-        image: {phone}
+        image: 'https://i.gadgets360cdn.com/large/google_pixel_4a_image_1596454112533.jpg?downsize=950:*&output-quality=80&output-format=webp'
     }
 ]
